dc4fb32aa312d6e6680b9f239836af67a1bff5fd
Zac <mymengyu@gmail.com>
2013-01-13 11:41:19 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meats::Application.config.secret_token = 'd595227ded8f6f929b3f1a5de9636b1479a2b8eb674e4edf25c1e474fe6c7ef9986b73179041b3ba5512fe0f257c275a9189f3b52c0ec8a5f5387ef2855e8785'
